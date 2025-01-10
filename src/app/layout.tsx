11de3935ac8ba060b6b8b0b5dc4b051fa9e34697
Gabriel Lulu <glulu4444@gmail.com>
2025-01-10 11:27:51 -0500

    edits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
     siteName: "Paragon Exterior",
     images: [
       {
-        url: "https://www.paragonexterior.com/images/roof-background-img.png",
+        url: "https://www.paragonexterior.com/public/images/roof-background-img.png",
         width: 1200,
         height: 630,
         alt: `Paragon Exterior`,
