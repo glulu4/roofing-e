2f21cbe8022bd7c6213af9a9e3539ff678dc502f
Gabriel Lulu <glulu4444@gmail.com>
2025-01-16 19:41:34 -0500

    changed text color

--- src/components/landing-ui/Quality.tsx
+++ src/components/landing-ui/Quality.tsx
@@ -15,10 +15,10 @@ export default function QualitySection({image, mainText, description}: QualitySe
         <div className="mx-auto w-11/12 px-6 lg:px-8 flex flex-col lg:flex-row items-center lg:items-stretch gap-8 bg-primaryblue p-10 rounded-sm">
 
             <div className="w-full lg:w-1/2 flex flex-col justify-center py-12">
-                <HeaderText className="mb-8 text-white">
+                <HeaderText className="mb-8 text-neutral-50">
                     {mainText}
                 </HeaderText>
-                <SecondaryText className=" text-gray-300">
+                <SecondaryText className=" text-neutral-100">
                     {description}
                 </SecondaryText>
             </div>
@@ -27,7 +27,7 @@ export default function QualitySection({image, mainText, description}: QualitySe
             <div className="w-full lg:w-1/2 flex items-center justify-center">
                 <img
                     src={image} // Replace with actual image URL
-                    alt="Roofing craftsmanship"
+                    alt="Paragon Exterior"
                     className="rounded-lg object-cover w-full h-full max-h-[500px]"
                 />
             </div>
