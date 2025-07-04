88263f5aa557badb56750776bce82a42b012fccd
Gabriel Lulu <glulu4444@gmail.com>
2025-07-04 14:08:46 -0400

    fixed gaf image path

--- src/components/StickyButton.tsx
+++ src/components/StickyButton.tsx
@@ -5,7 +5,7 @@ export default function StickyButton() {
         <div className="fixed bottom-4 left-4 z-50">
             <button className=" w-14 h-14 sm:w-16 sm:h-16 rounded-full shadow-lg transition-transform duration-200 hover:scale-110 active:scale-95 focus:outline-none">
                 <Image
-                    src="/images/gaf.png" // Replace with your image file path
+                    src="/images/home/company_icons/gaf.png" // Replace with your image file path
                     alt="GAF certification badge"
                     width={64} // Match the width of the button
                     height={64} // Match the height of the button
