ba6fbc43c5cf1cb39c4fff00e508c01238023218
Gabriel Lulu <glulu4444@gmail.com>
2025-07-08 15:03:14 -0400

    changed pella image

--- src/app/windows/page.tsx
+++ src/app/windows/page.tsx
@@ -14,7 +14,7 @@ const services = [
     title: 'Window Installation',
     href: '/windows/window-installation',
     description: 'Professional window installation services for energy-efficient and stylish windows. Upgrade your home with our expert team.',
-    imageUrl: '/images/window/window-install-card.webp',
+    imageUrl: '/images/window/window-installation-card.avif',
   },
   {
     id: 2,
@@ -40,48 +40,35 @@ export const metadata = {
 function page() {
   return (
     <div>
-
       <Hero
         mainText="Windows That Enhance Your Home's Beauty and Efficiency"
         subText="Upgrade your home with our premium window installation services. From energy-efficient windows to stylish designs, we offer solutions that improve comfort and curb appeal."
         imgSrc="/images/window/window-hero2.webp"
         imgAlt="Paragon Exterior window installation project"
       />
-
-
-
-
       <div className="flex flex-col justify-center sm:text-center items-center sm:w-5/6 mx-auto text-left px-8 py-32">
-
         <HeaderText as="h1" variant='large'>
           Trusted Window Contractor for Quality Solutions
         </HeaderText>
-
         <div className='my-12'>
           <SecondaryText>
             Choosing the right windows is essential for improving your home’s comfort, energy efficiency, and curb appeal. Well-designed windows help insulate your home, reduce energy loss, and ease the burden on your HVAC system in both hot summers and cold winters. Whether you’re upgrading outdated windows or looking for new window installations, Paragon Exterior will guide you in selecting stylish, durable solutions that enhance your home and help lower utility bills.
           </SecondaryText>
-
           <SecondaryText>
             At Paragon Exterior®, we’re more than just a window installation company — we’re your trusted partner in exterior improvement. From window replacement to custom window solutions, we help homeowners across Pennsylvania, New Jersey, and Delaware find high-quality products that provide natural light, fresh air, and superior protection from the elements. Let our expert team make the process simple and deliver results that transform your home’s look and performance.
           </SecondaryText>
         </div>
-
       </div>
-
-
-
       <div className='pb-12'>
         <ServicesGrid services={services} />
       </div>
       <HorizontalSlider />
-
       <WhyParagon
-      title='Why Choose Paragon Exterior for Your Window Needs?'
-      subTitle='At Paragon Exterior, we take pride in being your trusted window contractor. Our team of experts is dedicated to providing top-notch window installation, replacement, and repair services. We understand that windows are a crucial part of your home’s exterior, and we strive to deliver solutions that enhance both aesthetics and energy efficiency. With our commitment to quality craftsmanship and customer satisfaction, you can trust us to transform your home with beautiful, functional windows.'
+        title='Why Choose Paragon Exterior for Your Window Needs?'
+        subTitle='At Paragon Exterior, we take pride in being your trusted window contractor. Our team of experts is dedicated to providing top-notch window installation, replacement, and repair services. We understand that windows are a crucial part of your home’s exterior, and we strive to deliver solutions that enhance both aesthetics and energy efficiency. With our commitment to quality craftsmanship and customer satisfaction, you can trust us to transform your home with beautiful, functional windows.'
       />
 
-      <GetEstimate/>
+      <GetEstimate />
     </div>
   )
 }
